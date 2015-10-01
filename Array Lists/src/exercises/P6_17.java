95ab23bbe1fd5ef516c819c9053bd1c13337ce09
Jonas Nedergaard Andersen <jonas-na@hotmail.dk>
2015-10-01 18:45:03 +0200

    P6_17 done

--- Array Lists/src/exercises/P6_17.java
+++ Array Lists/src/exercises/P6_17.java
@@ -10,23 +10,25 @@ public class P6_17 {
 	
 	double max_value = 1000;
 	double asterisks_count = (40/max_value);
-	ArrayList<Double> inputs = new ArrayList<Double>();
+	ArrayList<String> inputs = new ArrayList<String>();
 	
 	System.out.println("Min value: 0 - Max value: 1000");
 	System.out.println("Inputs: ");
 	double input = scanner.nextDouble();
 	while (input > 0 && input <= max_value){
-	inputs.add(input);
-	input = scanner.nextDouble();
+		System.out.println("Input caption: ");
+		String s1 = scanner.next() + " ";
+		int temp = (int) (asterisks_count * input);
+		for (int i = 1; i <= temp; i++){
+			s1 += String.valueOf('*');
+			}
+		inputs.add(s1);
+		System.out.println("Input value: ");
+		input = scanner.nextDouble();
 	}
 	
-	for (double element : inputs){
-		int temp = (int) (asterisks_count * element);
-		String s1 = "";
-		for (int i = 1; i <= temp; i++){
-		s1 += String.valueOf('*');
-		}
-		System.out.println(s1);
+	for (String element : inputs){
+		System.out.println(element);
 	}
 	scanner.close();
 
